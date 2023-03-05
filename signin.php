181a9d051a0469464a1a62f45b5daeef232ff075
MhmdAtout <mohamad.atout0@icloud.com>
2023-03-05 19:13:56 +0200

    adding signin api

---
+++ signin.php
@@ -0,0 +1,70 @@
+<?php
+
+include('connection.php');
+
+if(isset($_POST["user_id"])){
+    $user_id = $_POST["user_id"];
+
+    $query = $mysql -> prepare("SELECT * FROM `users` Where user_id = ? ");
+    $query -> bind_param("i", $user_id);
+    $query -> execute();
+
+    while($object = $result -> fetch_assoc()){
+        $data = $object;
+    }
+
+    $response = [
+        "data" => $data
+    ];
+}else{
+    $credentials = $_POST["credentials"];
+    // $username = $_POST["username"];
+    // $email = $_POST["email"];
+
+    $password = $_POST["password"];
+    $hashed = hash("sha256", $password);
+
+    $query = $mysql -> prepare("SELECT * FROM `admins` WHERE `username`=? or `email`=? AND `password`=?");
+    $query -> bind_param("sss", $credentials, $credentials, $hashed);
+    $query -> execute();
+    $result = $query -> get_result();
+
+    while($object = $result -> fetch_assoc()){
+        $data = $object;
+    }
+
+    if(isset($data)){
+        $response = [
+            "id" => $data["admin_id"],
+            "user_type" => "admin",
+        ];
+    }else{
+        $query = $mysql -> prepare("SELECT * FROM `users` WHERE `username`=? or `email`=? AND `password`=?");
+        $query -> bind_param("sss", $credentials, $credentials, $hashed);
+        $query -> execute();
+        $result = $query -> get_result();
+    
+        while($object = $result -> fetch_assoc()){
+            $data = $object;
+        }
+    
+        if(isset($data)){
+                $response = [
+                    "id" => $data["user_id"],
+                    "username" => $data["username"],
+                    "first_name" => $data["first_name"],
+                    "last_name" => $data["last_name"],
+                    "user_type" => "customer",
+                ];
+        }else{
+            $response =[
+                "message" => "Credentials are incorrect"
+            ];
+        };
+    }
+
+}
+
+echo json_encode($response)
+
+?>
