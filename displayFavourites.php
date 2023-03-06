2b31b58d89c66fa9aa731186d9e8cf28b1d3b7c5
mariam-adsaleh <mariamadnansaleh@gmail.com>
2023-03-06 13:24:50 +0200

    fixing display favourites

--- displayFavourites.php
+++ displayFavourites.php
@@ -2,11 +2,12 @@
 include('connection.php');
 
 
-$user_id=$_GET['user_id'];
-$user_id=$_GET['product_id'];
+$user_id=$_POST['user_id'];
 
 
-$query = $mysqli->prepare('select product_id from favourites where user_id=?');
+
+$query = $mysql->prepare('select * from favourites f
+                            join products p on p.product_id = f.product_id where user_id=?');
 $query->bind_param('i', $user_id);
 $query->execute();
 $result = $query -> get_result();
@@ -14,16 +15,17 @@ $result = $query -> get_result();
 while ($object = $result -> fetch_assoc()){
     $data[] = $object;
 }
-if(isset($data)){
-    for($i=0; $i<count($data);$i++){
-        $query = $mysqli->prepare('select * from products where product_id=?');
-        $query->bind_param('i', $data[i]);
-        $query->execute();
-        $response+=[
-            "products"=>$data[i];
-        ];
-    }
-}
+$response["products"]=$data;
+// if(isset($data)){
+//     for($i=0; $i<count($data);$i++){
+//         $query = $mysqli->prepare('select * from products where product_id=?');
+//         $query->bind_param('i', $data[i]);
+//         $query->execute();
+//         $response+=[
+//             "products"=>$data[i];
+//         ];
+//     }
+// }
 
 // $response=["user"=>$data];
 echo json_encode($response);
