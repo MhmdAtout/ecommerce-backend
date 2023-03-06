e2687ff0195d3e5463f3641e9c73afd8f08b48da
mariam-adsaleh <mariamadnansaleh@gmail.com>
2023-03-06 12:53:13 +0200

    updating display Fav

--- displayFavourites.php
+++ displayFavourites.php
@@ -6,22 +6,26 @@ $user_id=$_GET['user_id'];
 $user_id=$_GET['product_id'];
 
 
-$query = $mysqli->prepare('select * from favourites where user_id=?');
+$query = $mysqli->prepare('select product_id from favourites where user_id=?');
 $query->bind_param('i', $user_id);
 $query->execute();
-
-$query = $mysqli->prepare('select * from products where product_id=?');
-$query->bind_param('i', $product_id);
-$query->execute();
- 
-
 $result = $query -> get_result();
 
 while ($object = $result -> fetch_assoc()){
-    $data = $object;
+    $data[] = $object;
+}
+if(isset($data)){
+    for($i=0; $i<count($data);$i++){
+        $query = $mysqli->prepare('select * from products where product_id=?');
+        $query->bind_param('i', $data[i]);
+        $query->execute();
+        $response+=[
+            "products"=>$data[i];
+        ];
+    }
 }
 
-$response=["user"=>$data];
+// $response=["user"=>$data];
 echo json_encode($response);
 
 
