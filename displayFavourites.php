10b8708d5b3469dd07f837f416f4c8401f1c22fd
mariam-adsaleh <mariamadnansaleh@gmail.com>
2023-03-06 13:33:15 +0200

    Cleaning code

--- displayFavourites.php
+++ displayFavourites.php
@@ -1,10 +1,7 @@
 <?php
 include('connection.php');
 
-
 $user_id=$_POST['user_id'];
-
-
 
 $query = $mysql->prepare('select * from favourites f
                             join products p on p.product_id = f.product_id where user_id=?');
@@ -16,19 +13,5 @@ while ($object = $result -> fetch_assoc()){
     $data[] = $object;
 }
 $response["products"]=$data;
-// if(isset($data)){
-//     for($i=0; $i<count($data);$i++){
-//         $query = $mysqli->prepare('select * from products where product_id=?');
-//         $query->bind_param('i', $data[i]);
-//         $query->execute();
-//         $response+=[
-//             "products"=>$data[i];
-//         ];
-//     }
-// }
-
-// $response=["user"=>$data];
 echo json_encode($response);
-
-
 ?>
--- displayUsers.php
+++ displayUsers.php
@@ -1,13 +1,8 @@
 <?php
 include('connection.php');
 
-
-
-
 $query = $mysql->prepare('select * from users');
 $query->execute();
-
-
 $result = $query -> get_result();
 
 while ($object = $result -> fetch_assoc()){
